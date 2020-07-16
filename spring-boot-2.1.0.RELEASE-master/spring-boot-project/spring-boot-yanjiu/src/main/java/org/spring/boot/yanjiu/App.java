df5a1a8cce7caafc9571d20681fddde43ce60eb8
everydatestudy <2767004352@qq.com>
2020-07-16 16:10:39 +0800

    提交文件

--- spring-boot-2.1.0.RELEASE-master/spring-boot-project/spring-boot-yanjiu/src/main/java/org/spring/boot/yanjiu/App.java
+++ spring-boot-2.1.0.RELEASE-master/spring-boot-project/spring-boot-yanjiu/src/main/java/org/spring/boot/yanjiu/App.java
@@ -4,14 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Hello world!
- *
+ * https://blog.csdn.net/qq_26000415/category_9271293.html
+ * 这个springboot的源码是1.5版本的 spring boot的源码分析
+ * 
+ * Hello world! 然后研究，spring的cloud的内容，nacos数据处理https://me.csdn.net/u010634066
  */
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
-        SpringApplication.run(App.class, args);
-    }
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
